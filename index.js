0e643d14845e12340f0a461d0d8501b844634749
Secret333Boy <pashkovskiy.eugen@gmail.com>
2022-01-12 20:09:27 +0200

    Root page created & engines fix

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 require('dotenv').config();
 require('./bot.js');
 const express = require('express');
+const path = require('path');
 
 const app = express();
 app.use(express.json());
+app.use(express.static(path.resolve(__dirname, 'client')));
+app.get('/', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'client', 'index.html'));
+});
 app.get('*', (req, res) => {
   res.status(404).send();
 });
