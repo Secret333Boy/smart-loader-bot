62b1549b3090a3e2fff084b9a078fea2053aeb1a
Secret333Boy <pashkovskiy.eugen@gmail.com>
2022-01-12 17:22:44 +0200

    Project renamed

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 require('dotenv').config();
 const { Telegraf } = require('telegraf');
-const bot = new Telegraf(process.env.BOT_TOKEN);
+const token = process.env.BOT_TOKEN;
+if (!token) throw new Error('Token should be provided');
+const bot = new Telegraf(token);
 bot.start((ctx) => {
   ctx.reply('Hey!');
 });
