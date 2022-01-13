43a5cdb4c62b68e6291a82f5ed5a5fffec2df0ee
Secret333Boy <pashkovskiy.eugen@gmail.com>
2022-01-14 01:25:41 +0200

    Added rateLimit

--- index.js
+++ index.js
@@ -2,9 +2,15 @@ require('dotenv').config();
 require('./bot.js');
 const express = require('express');
 const path = require('path');
+const rateLimit = require('express-rate-limit');
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 100,
+});
 
 const app = express();
 app.use(express.json());
+app.use(limiter);
 app.use(express.static(path.resolve(__dirname, 'client')));
 app.get('/', (req, res) => {
   res.sendFile(path.resolve(__dirname, 'client', 'index.html'));
